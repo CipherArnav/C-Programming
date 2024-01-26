d343b668f8009624f6b12837177a379dc213db97
CipherArnav <arnav14326@gmail.com>
2024-01-26 19:16:57 +0530

    Improved Hello Program

--- Java/Hello.java
+++ Java/Hello.java
@@ -2,6 +2,7 @@ class Hello
 {
   public static void main(String args [])
   {
-  System.out.println("This is my first Java Program");
+  System.out.println("This is my first Java Program"); // println is used to print the string inside the function and then move to next line
+  System.out.print("This is my first Java Program");
   }
 }
