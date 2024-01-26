ae99f93f404f16e1dcafb45b7cc9d4e09aad7f89
CipherArnav <arnav14326@gmail.com>
2024-01-26 19:25:04 +0530

    Updated

--- Java/Hello.java
+++ Java/Hello.java
@@ -3,5 +3,6 @@ class Hello
   public static void main(String args [])
   {
   System.out.println("This is my first Java Program"); // println is used to print the string inside the function and then move to next line.
+  System.out.print("This is my first Java Program");
   }
 }
