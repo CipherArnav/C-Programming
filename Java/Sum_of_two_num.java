5a9f08a4853f02bd75b07e6018fd4be2907ffb0b
CipherArnav <arnav14326@gmail.com>
2024-01-30 23:44:59 +0530

    Save changes

---
+++ Java/Sum_of_two_num.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Sum_of_two_num 
+{
+    public static void main(String args[])
+{
+   System.out.println("Enter the value of a and b:");
+   Scanner sc = new Scanner(System.in);
+   int a = sc.nextInt();
+   int b = sc.nextInt();
+   int sum = a+b;
+   System.out.print("Your sum is:");
+   System.out.print(sum);
+
+
+}
+
+}
