0cabf02828e73b28fea983478b33c83c34545205
Arnav <153320445+CipherArnav@users.noreply.github.com>
2024-05-06 01:27:46 +0530

    Delete C-Programming directory

--- C-Programming/Loops/Basicsofloops.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    for (int i=1;i<=5;i=i+2)
-    {
-        printf("Hello World\n");
-    }
-    return 0;
-    
-}
--- C-Programming/Loops/Postpre.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int x=5;
-    printf("%d \n",x);
-    printf("%d",++x); // x++ means , use x, then increment
-    // ++x means , increment then use.
-    return 0;
-}
